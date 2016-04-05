d6fd799db99dac96532b67e584e7bdab9601e338
float1251 <taka.05022002@gmail.com>
2016-04-05 16:24:21 +0900

    render multiple models

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ declare var mat4: any;
 
 let VSHADER_SRC = `
 attribute vec3 a_Position;
+attribute vec4 a_Color;
 uniform   mat4 mvpMatrix;
+varying vec4 v_Color;
 
 void main(){
-   gl_Position = mvpMatrix * vec4(a_Position, 1.0);
+    v_Color = a_Color; 
+    gl_Position = mvpMatrix * vec4(a_Position, 1.0);
 }`;
 
-let FSHADER_SRC = `void main(){
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);   
+let FSHADER_SRC = `
+precision mediump float;
+varying vec4 v_Color;
+void main(){
+    gl_FragColor = v_Color;   
 }`;
 
 function main() {
@@ -31,10 +37,14 @@ function main() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // attributeLocationの取得
-    var attLocation = gl.getAttribLocation(program, "a_Position");
+    var attLocation = new Array(2);
+    attLocation[0] = gl.getAttribLocation(program, "a_Position");
+    attLocation[1] = gl.getAttribLocation(program, "a_Color");
 
     // attributeの要素数(この要素はxyzの3要素)
-    var attStride = 3;
+    var attStride = new Array(2);
+    attStride[0] = 3;
+    attStride[1] = 4;
 
     // 頂点データ
     var vertex_position = [
@@ -43,35 +53,58 @@ function main() {
        -1.0, 0.0, 0.0 
     ];
 
-    var vbo = createVBO(gl, vertex_position);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    var vertex_color = [
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0
+    ]
+
+    var position_vbo = createVBO(gl, vertex_position);
+    var color_vbo = createVBO(gl, vertex_color);
     // attribute属性を有効にする
-    gl.enableVertexAttribArray(attLocation);
     // attribute属性を登録
-    gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, position_vbo);
+    gl.enableVertexAttribArray(attLocation[0]);
+    gl.vertexAttribPointer(attLocation[0], attStride[0], gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_vbo);
+    gl.enableVertexAttribArray(attLocation[1]);
+    gl.vertexAttribPointer(attLocation[1], attStride[1], gl.FLOAT, false, 0, 0);
+
 
     // 行列変換処理
     var mMatrix = mat4.create();
     var vMatrix = mat4.create();
     var pMatrix = mat4.create();
+    var tmpMatrix = mat4.create();
     var mvpMatrix = mat4.create();
 
     // ビュー変換行列
-    mat4.lookAt(vMatrix, [0.0, 1.0, 3.0], [0, 0, 0], [0,1,0]);
-
+    mat4.lookAt(vMatrix, [0.0, 0.0, 3.0], [0, 0, 0], [0,1,0]);
     // プロジェクション座標変換行列
     mat4.perspective(pMatrix, 90, canvas.width/canvas.height, 0.1, 1000);
+    mat4.mul(tmpMatrix, pMatrix, vMatrix);
 
-    // 各行列を掛けあわせ、座標変換行列を関係させる
-    mat4.mul(mvpMatrix, pMatrix, vMatrix);
-    mat4.mul(mvpMatrix, mvpMatrix, mMatrix);
+    // 1つめのモデルを移動するためのモデル座標変換行列
+    mat4.translate(mMatrix, mMatrix, [1.5, 0.0, 0.0]);
+
+    // 各行列を掛けあわせ、座標変換行列を完成させる
+    mat4.mul(mvpMatrix, tmpMatrix, mMatrix);
 
     // uniformLocationの取得
     var uniLocation = gl.getUniformLocation(program, "mvpMatrix");
+    // uniformLocationへの座標変換行列を登録
+    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    mat4.identity(mMatrix);
+    mat4.translate(mMatrix, mMatrix, [-1.5, 0.0, 0.0]);
+
+    // モデルxビューxプロジェクション
+    mat4.mul(mvpMatrix, tmpMatrix, mMatrix);
 
     // uniformLocationへの座標変換行列を登録
     gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-
     gl.drawArrays(gl.TRIANGLES, 0, 3);
 
     // コンテキストの再描画
