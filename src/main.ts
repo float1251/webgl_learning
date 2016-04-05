2573b110a0e0b27dd78db298fc5ded921a305359
float1251 <taka.05022002@gmail.com>
2016-04-05 17:05:15 +0900

    Use IBO

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ void main(){
     gl_FragColor = v_Color;   
 }`;
 
+
 function main() {
     var canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("webgl");
     if(!canvas){
@@ -25,16 +26,19 @@ function main() {
         return;
     }
 
-    var gl = getWebGLContext(canvas);
-    gl.clearColor(0, 0, 0, 1);
-    gl.clearDepth(1.0);
+    var gl: WebGLRenderingContext;
+    // 行列変換処理
+    var mMatrix = mat4.create();
+    var vMatrix = mat4.create();
+    var pMatrix = mat4.create();
+    var tmpMatrix = mat4.create();
+    var mvpMatrix = mat4.create();
 
+    gl = getWebGLContext(canvas);
+    
     var program = initShaders(gl, VSHADER_SRC, FSHADER_SRC);
 
     var vertexPositionAttribute = gl.getAttribLocation(program, "a_Position");
-
-        // キャンバスの初期化
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // attributeLocationの取得
     var attLocation = new Array(2);
@@ -50,13 +54,20 @@ function main() {
     var vertex_position = [
         0.0, 1.0, 0.0,
         1.0, 0.0, 0.0,
-       -1.0, 0.0, 0.0 
+       -1.0, 0.0, 0.0,
+        0.0, -1.0, 0.0
     ];
 
     var vertex_color = [
         1.0, 0.0, 0.0, 1.0,
         0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0
+        0.0, 0.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0
+    ]
+
+    var index = [
+        0, 1, 2,
+        1, 2, 3
     ]
 
     var position_vbo = createVBO(gl, vertex_position);
@@ -65,12 +76,13 @@ function main() {
     // attribute属性を登録
     set_attribute(gl, [position_vbo, color_vbo], attLocation, attStride);
 
-    // 行列変換処理
-    var mMatrix = mat4.create();
-    var vMatrix = mat4.create();
-    var pMatrix = mat4.create();
-    var tmpMatrix = mat4.create();
-    var mvpMatrix = mat4.create();
+    // iboの作成
+    var ibo = create_ibo(gl, index);
+
+    // IBOをバインドして登録する
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+    // uniformLocationの取得
+    var uniLocation = gl.getUniformLocation(program, 'mvpMatrix');
 
     // ビュー変換行列
     mat4.lookAt(vMatrix, [0.0, 0.0, 3.0], [0, 0, 0], [0,1,0]);
@@ -78,30 +90,59 @@ function main() {
     mat4.perspective(pMatrix, 90, canvas.width/canvas.height, 0.1, 1000);
     mat4.mul(tmpMatrix, pMatrix, vMatrix);
 
-    // 1つめのモデルを移動するためのモデル座標変換行列
-    mat4.translate(mMatrix, mMatrix, [1.5, 0.0, 0.0]);
+    var render_count:number = 0;
+    function render_update(timestamp: any) {
+        // キャンバスの初期化
+        gl.clearColor(0, 0, 0, 1);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    
+        // カウントをインクリメント
+        render_count++;
 
-    // 各行列を掛けあわせ、座標変換行列を完成させる
-    mat4.mul(mvpMatrix, tmpMatrix, mMatrix);
+        // ラジアンを算出
+        var rad = (render_count%360) * Math.PI / 180;
+        
+        // モデル1は円の動きを描き、移動する
+        var x = Math.cos(rad);
+        var y = Math.sin(rad);
+        mat4.identity(mMatrix);
+        mat4.translate(mMatrix, mMatrix, [x, y+1.0, 0.0]);
+    
+        // モデル1の座標変換行列と、レンダリング
+        // モデルxビューxプロジェクション
+        mat4.mul(mvpMatrix, tmpMatrix, mMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
 
-    // uniformLocationの取得
-    var uniLocation = gl.getUniformLocation(program, "mvpMatrix");
-    // uniformLocationへの座標変換行列を登録
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+        // モデル2はY軸を中心に回転する
+        mat4.identity(mMatrix);
+        mat4.translate(mMatrix, mMatrix, [1.0, -1.0, 0.0]);
+        mat4.rotateY(mMatrix, mMatrix, rad);    
 
-    mat4.identity(mMatrix);
-    mat4.translate(mMatrix, mMatrix, [-1.5, 0.0, 0.0]);
+        // モデル2の座標変換行列と描画
+        mat4.mul(mvpMatrix, tmpMatrix, mMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
 
-    // モデルxビューxプロジェクション
-    mat4.mul(mvpMatrix, tmpMatrix, mMatrix);
+        // モデル3は拡大縮小する
+        var s = Math.sin(rad) + 1.0;
+        mat4.identity(mMatrix);
+        mat4.translate(mMatrix, mMatrix, [-1.0, -1.0, 0.0]);
+        mat4.scale(mMatrix, mMatrix, [s, s, s]);
+        
+        // モデル3の座標変換行列と描画
+        mat4.mul(mvpMatrix, tmpMatrix, mMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
 
-    // uniformLocationへの座標変換行列を登録
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.flush();
 
-    // コンテキストの再描画
-    gl.flush();
+        window.requestAnimationFrame(render_update);
+    }
+
+    // requestAnimationFrameの取得
+    window.requestAnimationFrame(render_update);
 }
 
 function getWebGLContext(canvas: HTMLCanvasElement): WebGLRenderingContext {
@@ -162,5 +203,18 @@ function set_attribute(gl: WebGLRenderingContext, vbo: Array<WebGLRenderbuffer>,
         // attributeLocationを通知し登録する
         gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
     }
+}
+
+// IBOを作成する
+function create_ibo(gl: WebGLRenderingContext, data: Array<number>): WebGLBuffer{
+    // バッファの作成
+    var ibo = gl.createBuffer()
+    // バッファのバインド
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+    // バッファにデータをセット
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+    // バッファの無効化
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    return ibo;
 }
 main();
